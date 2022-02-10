382eaa6f8c782a2c1eef178ff9adcda6c97151e1
Jenny <jenny.hutchins@warwick.ac.uk>
2022-02-10 13:19:52 +0000

    time tutorial connected front and back ends

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,8 @@
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
