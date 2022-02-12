36fad528e576867affa13bc2809dd4ea0a04e821
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-12 22:27:32 +0000

    Database info now stored in environment variables

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,22 +1,14 @@
 import psycopg2
+import os
 from psycopg2 import Error
 
 # TODO: Look into implementing a pool system for databse connections. https://www.psycopg.org/docs/pool.html
 
 def db_conn():
     print("Connecting to the database")
-
-    # Get database login info from file
-    try:
-        with open('../PostgresLogin') as f:
-            name = f.readline()[:-1]
-            passwd = f.readline()
-    except:
-        print("Database connection failed. Couldn't find PostgresLogin file.")
-
     global connection
     try:
-        connection = psycopg2.connect(dbname=name, user="postgres", password=passwd, host="127.0.0.1", port="5432")
+        connection = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), host="127.0.0.1", port="5432")
         print("Connection successful")
     except (Exception, Error) as error:
         print("Error while connecting to database", error)
