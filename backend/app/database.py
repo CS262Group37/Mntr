4024b11c1729a120da7c7be4a62b8cf5f0aebc4f
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-13 00:46:17 +0000

    Implemented basic database building

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,18 +1,28 @@
-import psycopg2
 import os
+import psycopg2
 from psycopg2 import Error
+from config import APP_ROOT
 
 # TODO: Look into implementing a pool system for databse connections. https://www.psycopg.org/docs/pool.html
 
-def db_conn():
+def connect():
     print("Connecting to the database")
-    global connection
+    global conn
     try:
-        connection = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), host="127.0.0.1", port="5432")
+        conn = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), host="127.0.0.1", port="5432")
         print("Connection successful")
     except (Exception, Error) as error:
         print("Error while connecting to database", error)
 
-def db_close():
+def close():
     print("Closing the database")
-    connection.close()
+    conn.close()
+
+# Executes schema.sql on the database
+def build():
+    print("Building database")
+    sqlFile = open(os.path.join(APP_ROOT, 'app', 'schema.sql'), "r")
+    with conn.cursor() as curs:
+        curs.execute(sqlFile.read())
+        conn.commit()
+    print("Finished building")
---
+++ backend/app/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS "user" CASCADE;
+
+CREATE TABLE "user" (
+    userID SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE,
+    "password" VARCHAR NOT NULL,
+    firstName VARCHAR NOT NULL,
+    lastName VARCHAR NOT NULL
+);
