a81508df858786568661e1116aa2f84c21faaca5
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-13 11:23:05 +0000

    Removed unneeded APP_ROOT var

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,3 @@
-import os
-
 # We can create multiple config classes in here to easily switch between them
 # Remember to change the FLASK_ENV variable in the .env file
 class DevConfig:
@@ -8,5 +6,4 @@ class DevConfig:
 
 class ProdConfig:
     DEBUG = False
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+    
