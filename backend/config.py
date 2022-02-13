5c6834009ecb9e0e92f7d802f6217b7e89e4b120
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-13 00:44:18 +0000

    Added APP_ROOT variable

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
+import os
+
 # We can create multiple config classes in here to easily switch between them
 # Remember to change the FLASK_ENV variable in the .env file
 class DevConfig:
@@ -6,3 +8,5 @@ class DevConfig:
 
 class ProdConfig:
     DEBUG = False
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
