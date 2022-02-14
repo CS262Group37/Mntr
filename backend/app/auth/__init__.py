102927fde6d2a35d3710ec69cab4ce0bfb5e4b96
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-14 00:11:19 +0000

    Added /api/ prefix to auth routes

--- backend/app/auth/__init__.py
+++ backend/app/auth/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint                            
 from flask_restful import Api
                                                        
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth') 
+auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth') 
 auth_api = Api(auth_bp)
 
 # Initialise the routes                                                                            
