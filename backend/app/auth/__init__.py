dfa3675e23821bca1e081a18b44efe125d7cc568
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-14 14:41:20 +0000

    Cleaned-up package imports and implemented parsers

--- backend/app/auth/__init__.py
+++ backend/app/auth/__init__.py
@@ -4,5 +4,5 @@ from flask_restful import Api
 auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth') 
 auth_api = Api(auth_bp)
 
-# Initialise the routes                                                                            
-from app.auth import routes
+# Initialise the routes                                                                         
+from . import routes
--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,10 +1,11 @@
-from flask_restful import Resource 
+from flask_restful import Resource
 from flask import request
-from flask import Response
+from flask import make_response
 from flask import abort
 from functools import wraps
-from app.auth import auth_api
-from app.auth import auth
+from . import auth_api
+from .parsers import *
+from .auth import *
 
 # Use this resource for any API call that requires authentication
 def authenticate(func):
@@ -21,7 +22,7 @@ def authenticate(func):
         if not hasattr(func.__self__, 'roles'):
             abort(401)
 
-        if auth.check_token(token, getattr(func.__self__, 'roles')):
+        if check_token(token, getattr(func.__self__, 'roles')):
             return func(*args, **kwargs)
         else:
             # TODO: Reply with WWW-Authenticate response header
@@ -40,25 +41,25 @@ class AuthResource(Resource):
 class Register(Resource):
 
     def post(self):
-        
-        auth.register_user(request.json)
+        data = register_parser.parse_args()
+        register_user(data)
         # TODO: Need registration error handling
         return {'message': 'Registered user successfully'}, 201
 
 class Login(Resource):
     
     def post(self):
+        data = login_parser.parse_args()
         
-        email = request.json.get('email')
-        password = request.json.get('password')
-        #args = login_parser.parse_args(strict=True)
-        
-        if not auth.check_email(email):
+        if not check_email(data['email']):
             return {'error': 'Email does not exist!'}, 401
 
         # TODO: Do password hashing verification stuff here
         # TODO: Need a way to store the generated auth token in a cookie or local storage
-        return Response(headers={'Authorization': auth.generate_token(auth.get_userID(email), auth.get_role(email))})
+        response = make_response()
+        response.headers['Authorization'] = generate_token(data['email'])
+        #return Response(headers={'Authorization': generate_token(get_userID(email), get_role(email))})
+        return response
 
 # Just for testing authentication. Provide the user's jwt token in the url.
 # They will be able to see a list of users on the system if they are authenticated as an admin
@@ -67,7 +68,7 @@ class PrintUsers(AuthResource):
     roles = ['admin']
 
     def get(self):
-        return auth.get_registered_users()
+        return get_registered_users()
 
 auth_api.add_resource(Register, '/register')
 auth_api.add_resource(Login, '/login')
