3639fc358519cb8bc6ae2f2ee3fd30806f354795
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-15 18:05:25 +0000

    Implemented a DatabaseConnection object

--- backend/app/database.py
+++ backend/app/database.py
@@ -23,41 +23,49 @@ def close_connection_pool():
     print("Closing the database")
     app.db_pool.closeall()
 
-# Context manager to getting a connection from the pool. Use inside a with clause
-class Connection(object):
-    def __init__(self, pool):
-        self.pool = pool
-        self.conn = pool.getconn()
+# Use this class in a with clause for executing sql on the database. The execute function will return
+# the result of the SQL query in a data structure. NOTE: When using this class make sure to check
+# the value of self.error at the end of the with clause. It will be true if an SQL error occurs in the
+# class's lifetime. SQL errors are otherwised suppressed to keep things simple (I might change this eventually)
+class DatabaseConnection(object):
+
+    def __init__(self):
+        self.conn = app.db_pool.getconn()
+        self.curs = self.conn.cursor()
+        self.error = False
 
     def __enter__(self):
-        return self.conn
+        return self
 
+    # Closes the cursor and connection
     def __exit__(self, type, value, traceback):
-        self.pool.putconn(self.conn)
+        self.curs.close()
+        app.db_pool.putconn(self.conn)
+
+    # Use this for manually closing the connection when not using a context manager
+    def close(self):
+        self.__exit__()
+    
+    # Pass sql in the form of a string data in the form of a tuple. NOTE: If you are passing in a
+    # single variable, it needs to have a comma after for some reason e.g. (variable, )
+    def execute(self, sql, data = None):
+        fetch = None
+        try:
+            self.curs.execute(sql, data)
+            try:
+                fetch = self.curs.fetchall()
+            except:
+                pass
+            self.conn.commit()
+        except Error as e:
+            print(e)
+            self.conn.rollback()
+            self.error = True
+        return fetch
 
 # Executes schema.sql on the database
-def build():
-    print("Building database")
+def load_schema():
     # TODO: Use instance folders to store schema.sql https://flask.palletsprojects.com/en/0.12.x/config/#instance-folders
     sqlFile = open(os.path.join(app.root_path, 'schema.sql'), "r")
-    execute(sqlFile.read())
-    print("Finished building")
-
-def execute(sql, data = None):
-    result = None
-    with Connection(app.db_pool) as conn:
-        with conn.cursor() as curs:
-            try:
-                curs.execute(sql, data)
-                try:
-                    result = curs.fetchall()
-                except:
-                    pass
-                conn.commit()
-            except Error as e:
-                print(e)
-                conn.rollback()
-                raise # TODO: Test this method of error handling
-    return result
-
-                
+    with DatabaseConnection() as db:
+        db.execute(sqlFile.read())
