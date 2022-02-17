4f877b9ed9abebc2802d17df34677fb196f4ef5e
Jenny <jenny.hutchins@warwick.ac.uk>
2022-02-17 12:34:17 +0000

    use token to get users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,22 +6,39 @@ import axios from "axios";
 function App() {
   const [email, setEmail] = React.useState<string>("");
   const [psword, setPsword] = React.useState<string>("");
+  const [token, setToken] = React.useState<string>("");
 
+  const login = async () => {
+    const res = await axios.post("/api/auth/login", {
+      email: email,
+      password: psword,
+    });
+    setToken(res.headers.authorization);
+  };
 
-  const login = () => {
-    axios
-      .post("/api/auth/login", {
-        email: email,
-        password: psword,
-      })
-      .then((res) => console.log(res));
+  const getUsers = async () => {
+    const headers = {
+      authorization: token,
+    };
+    const res = await axios.get("/api/auth/users", { headers });
+    console.log(res);
   };
 
   return (
     <div className="App">
       <button onClick={login}>Login</button>
-      <input onChange={(e) => {setEmail(e.target.value)}}></input>
-      <input onChange={(e) => {setPsword(e.target.value)}}></input>
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      ></input>
+      <input
+        onChange={(e) => {
+          setPsword(e.target.value);
+        }}
+      ></input>
+      <p>{token}</p>
+      <button onClick={getUsers}>Users</button>
     </div>
   );
 }
