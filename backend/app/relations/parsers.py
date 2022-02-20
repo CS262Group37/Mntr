7163da8ec278b82c15ffda40e1edf0576570cfe7
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-20 16:03:13 +0000

    Added multiple new relation routes

--- backend/app/relations/parsers.py
+++ backend/app/relations/parsers.py
@@ -4,3 +4,7 @@ send_email_parser = reqparse.RequestParser(bundle_errors=True)
 send_email_parser.add_argument('recipientID', required=True, type=int)
 send_email_parser.add_argument('senderID', required=True, type=int)
 send_email_parser.add_argument('contents', required=True, type=str)
+
+# The mentee should send this request themselves so we can get their ID from cookie
+create_relation_parser = reqparse.RequestParser(bundle_errors=True)
+create_relation_parser.add_argument('mentorID', required=True, type=int)
