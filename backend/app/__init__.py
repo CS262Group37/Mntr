244f8e1e7ab7ea7cece0340336648a0cd1f4af28
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-22 18:17:50 +0000

    Converted app entry to factory pattern

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,37 +1,31 @@
 from flask import Flask
-from flask_restful import Api
+
 import config
+import app.auth as auth
+import app.matching as matching
+import app.meetings as meetings
+import app.messages as messages
+import app.relations as relations
+from . import database as db
 
-# TODO: Eventually switch this to a factory design pattern but this is fine for now
+# Good description of the pattern I'm trying to implement: http://exploreflask.com/en/latest/blueprints.html
 
-# Create app
-app = Flask(__name__)
-api = Api(app)
+def create_app():
+    """Initialise the application."""
+    app = Flask(__name__)
+    
+    app.config.from_object(config.DevConfig)
 
-# Apply desired config from config.py
-app.config.from_object(config.DevConfig)
+    # Initialise database
+    with app.app_context():
+        db.init_db()
+        db.load_schema()
+    
+    # Register blueprints
+    app.register_blueprint(auth.auth_bp, url_prefix='/auth', subdomain='api')
+    app.register_blueprint(matching.matching_bp, url_prefix='/matching', subdomain='api')
+    app.register_blueprint(meetings.meetings_bp, url_prefix='/meetings', subdomain='api')
+    app.register_blueprint(messages.messages_bp, url_prefix='/messages', subdomain='api')
+    app.register_blueprint(relations.relations_bp, url_prefix='/relations', subdomain='api')
 
-# Initialise routes                                                                            
-from app import routes
-
-# Connect to the database
-from . import database
-with app.app_context():
-    database.create_connection_pool()
-    database.load_schema()
-
-# Register blueprints
-from app.auth import auth_bp
-app.register_blueprint(auth_bp)
-
-from app.matching import matching_bp
-app.register_blueprint(matching_bp)
-
-from app.meetings import meetings_bp
-app.register_blueprint(meetings_bp)
-
-from app.messages import messages_bp
-app.register_blueprint(messages_bp)
-
-from app.relations import relations_bp
-app.register_blueprint(relations_bp)
+    return app
