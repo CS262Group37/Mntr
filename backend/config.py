f30032bfce77b7aad554695d6755989bd6fddb09
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-22 18:16:54 +0000

    Added setting for custom server name

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,11 @@
+import os
 # We can create multiple config classes in here to easily switch between them
 # Remember to change the FLASK_ENV variable in the .env file
 class DevConfig:
-    DEBUG = True
     SECRET_KEY = 'dev'
+    SERVER_NAME = os.getenv('HOSTNAME')
 
 class ProdConfig:
-    DEBUG = False
+    #SERVER_NAME = 'mydomain.com:5000'
+    SECRET_KEY = 'dev' # TODO: Make this a crazy random string for actual deployment
     
