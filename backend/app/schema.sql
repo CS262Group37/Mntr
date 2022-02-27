472c7110d313da4d6b212446bc9459b66178d9fb
Joshua Manchester <joshuamanchester4@gmail.com>
2022-02-27 17:54:55 +0000

    Added tables for user topics and ratings

--- backend/app/schema.sql
+++ backend/app/schema.sql
@@ -1,11 +1,12 @@
-DROP TABLE IF EXISTS account CASCADE;
-DROP TABLE IF EXISTS "user" CASCADE;
-DROP TABLE IF EXISTS "message" CASCADE;
-DROP TABLE IF EXISTS relation CASCADE;
 DROP TABLE IF EXISTS system_business_area CASCADE;
 DROP TABLE IF EXISTS system_topic CASCADE;
 DROP TABLE IF EXISTS system_skill CASCADE;
-DROP TABLE IF EXISTS rating CASCADE;
+DROP TABLE IF EXISTS account CASCADE;
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS user_topic CASCADE;
+DROP TABLE IF EXISTS user_rating CASCADE;
+DROP TABLE IF EXISTS "message" CASCADE;
+DROP TABLE IF EXISTS relation CASCADE;
 DROP TABLE IF EXISTS report CASCADE;
 
 -- Constraint functions --
@@ -15,6 +16,16 @@ DROP TRIGGER IF EXISTS add_relation_contraints ON relation;
 CREATE TABLE system_business_area (
     businessAreaID SERIAL PRIMARY KEY,
     "name" VARCHAR NOT NULL CONSTRAINT unique_area_name UNIQUE
+);
+
+CREATE TABLE system_topic (
+    topicID SERIAL PRIMARY KEY,
+    "name" VARCHAR NOT NULL CONSTRAINT unique_topic UNIQUE
+);
+
+CREATE TABLE system_skill (
+    skillID SERIAL PRIMARY KEY,
+    "name" VARCHAR NOT NULL CONSTRAINT unique_skill UNIQUE
 );
 
 CREATE TABLE account (
@@ -29,8 +40,22 @@ CREATE TABLE "user" (
     userID SERIAL PRIMARY KEY,
     accountID INTEGER REFERENCES account(accountID),
     "role" VARCHAR NOT NULL CONSTRAINT invalid_role CHECK ("role" IN ('mentee', 'mentor', 'admin')),
-    businessArea VARCHAR NOT NULL CONSTRAINT valid_business_area REFERENCES system_business_area("name"),
+    -- businessArea can be NULL for admin users
+    businessArea VARCHAR CONSTRAINT valid_business_area REFERENCES system_business_area("name"),
     CONSTRAINT one_role_per_account UNIQUE (accountID, "role")
+);
+
+CREATE TABLE user_topic (
+    userID INTEGER NOT NULL REFERENCES "user"(userID),
+    topicID INTEGER NOT NULL CONSTRAINT valid_topic REFERENCES system_topic(topicID),
+    PRIMARY KEY (userID, topicID)
+);
+
+CREATE TABLE user_rating (
+    userID INTEGER NOT NULL REFERENCES "user"(userID),
+    skillID INTEGER NOT NULL CONSTRAINT valid_skill REFERENCES system_skill(skillID),
+    rating INTEGER NOT NULL CONSTRAINT valid_rating CHECK (rating >= 0 AND rating <= 10),
+    PRIMARY KEY (userID, skillID)
 );
 
 CREATE TABLE "message" (
@@ -38,7 +63,7 @@ CREATE TABLE "message" (
     recipientID INTEGER NOT NULL REFERENCES "user"(userID),
     senderID INTEGER NOT NULL REFERENCES "user"(userID),
     messageType VARCHAR NOT NULL CONSTRAINT valid_message_type CHECK (messageType IN ('email', 'feedback', 'report')),
-    sentTime TIMESTAMP NOT NULL, 
+    sentTime TIMESTAMP NOT NULL,
     CONSTRAINT distinct_recipient_and_sender CHECK (recipientID <> senderID)
 );
 
@@ -49,23 +74,6 @@ CREATE TABLE relation (
     menteeID INTEGER NOT NULL REFERENCES "user"(userID),
     mentorID INTEGER NOT NULL REFERENCES "user"(userID),
     CONSTRAINT unique_relation UNIQUE (mentorID, menteeID)
-);
-
-CREATE TABLE system_topic (
-    topicID SERIAL PRIMARY KEY,
-    "name" VARCHAR NOT NULL CONSTRAINT unique_topic UNIQUE
-);
-
-CREATE TABLE system_skill (
-    skillID SERIAL PRIMARY KEY,
-    "name" VARCHAR NOT NULL CONSTRAINT unique_skill UNIQUE
-);
-
--- Contains user's ratings for skills
-CREATE TABLE rating (
-    userID INTEGER NOT NULL REFERENCES "user"(userID),
-    skillID INTEGER NOT NULL REFERENCES system_skill(skillID),
-    rating INTEGER NOT NULL CONSTRAINT valid_rating CHECK (rating >= 0 AND rating <= 10)
 );
 
 CREATE TABLE report (
