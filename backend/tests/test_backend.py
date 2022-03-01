009e200f0783d8681c54bb7d3e44112e30358c1f
istharryvarry <55281309+istharryvarry@users.noreply.github.com>
2022-03-01 04:34:19 +0000

    Tests

---
+++ backend/tests/test_backend.py
@@ -0,0 +1,35 @@
+from app.admin import admin
+from app.auth import auth
+from app.matching import matching
+from app.meetings import meetings
+from app.messages import messages
+from app.planOfAction import planOfAction
+from app.relations import relations
+
+def test_backend():
+    # C1
+    assert admin.add_topic() is 
+    # C2
+    assert admin.add_topic() is 
+    # C3
+    assert admin.add_topic() is 
+    # C4
+    assert admin.add_topic() is 
+    # C5
+    assert admin.add_topic() is 
+    # C6
+    assert admin.add_topic() is 
+    # C7
+    assert admin.add_topic() is 
+    # C8
+    assert admin.add_topic() is 
+    # C9
+    assert admin.add_topic() is 
+    # C10
+    assert admin.add_topic() is 
+    # C11
+    assert admin.add_topic() is 
+    # D12
+    assert admin.add_topic() is 
+    # C13
+    assert admin.add_topic() is 
