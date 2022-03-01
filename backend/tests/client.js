c9a2548f07f575ab0b3516fe677ebfdfec2fe9ec
istharryvarry <55281309+istharryvarry@users.noreply.github.com>
2022-03-01 02:03:29 +0000

    Create client.js

---
+++ backend/tests/client.js
@@ -0,0 +1,9 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
