e19edfb2aa0ebc00ec5af690eed5670d3442002e
Joshua Manchester <joshuamanchester4@gmail.com>
2022-03-04 17:27:37 +0000

    Finished random meeting generation

--- backend/tests/UserGenerator/UserGenerator/meetings.py
+++ backend/tests/UserGenerator/UserGenerator/meetings.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import random
+from datetime import datetime, timedelta
+from time import perf_counter
 
 from rich.table import Table
 from rich.progress import Progress
@@ -10,6 +12,7 @@ from rich import box
 from .console import add_option, console, hostname
 from . import authentication
 from .database import get_data
+from .fake_data import fake
 
 def add_meeting(relationID, startTime, endTime, title, description):
 
@@ -34,6 +37,7 @@ def create_random_meetings(meeting_count = None):
         meeting_count = IntPrompt.ask('Enter the number of random meetings to add')
         console.line()
     created_meetings = 0
+    start = perf_counter()
 
     # Get all relations on the system
     relations = get_data('SELECT * FROM relation')
@@ -42,5 +46,13 @@ def create_random_meetings(meeting_count = None):
         
         for i in range(meeting_count):
             relation = random.choice(relations)
-            random_time = random.randrange()
-            add_meeting(relation, )
+            random_time = datetime.today() + random.random() * timedelta(days=1)
+            random_end_time = random_time + timedelta(minutes=random.randrange(120))
+            if add_meeting(relation, random_time.strftime('%d/%m/%y %H:%M'), random_end_time.strftime('%d/%m/%y %H:%M'), fake.sentences(nb=1), fake.paragraph(nb_sentences=5)):
+                created_meetings += 1
+            progress.update(meeting_progress, advance=1)
+    stop = perf_counter()
+    console.print(f'\n[green]Successfully created {created_meetings} meetings in {stop - start} seconds[/]')
+
+def add_options():
+    add_option('meetings', 'Add random meetings', create_random_meetings)
