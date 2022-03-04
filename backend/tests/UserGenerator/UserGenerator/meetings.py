a7c6c80fb23776e72a36156c554ea4a8ded05bbc
Joshua Manchester <joshuamanchester4@gmail.com>
2022-03-04 22:25:47 +0000

    Finished random meeting generation

--- backend/tests/UserGenerator/UserGenerator/meetings.py
+++ backend/tests/UserGenerator/UserGenerator/meetings.py
@@ -1,15 +1,14 @@
 import requests
 import json
 import random
-from datetime import datetime, timedelta
+from datetime import timedelta
 from time import perf_counter
 
-from rich.table import Table
 from rich.progress import Progress
 from rich.prompt import IntPrompt
-from rich import box
 
 from .console import add_option, console, hostname
+from .authentication import login_user
 from . import authentication
 from .database import get_data
 from .fake_data import fake
@@ -23,7 +22,7 @@ def add_meeting(relationID, startTime, endTime, title, description):
             'endTime': endTime,
             'title': title,
             'description': description
-        }, timeout=10
+        }, timeout=10, cookies=authentication.active_cookie
         )
     
     data = json.loads(response.content)
@@ -46,7 +45,14 @@ def create_random_meetings(meeting_count = None):
         
         for i in range(meeting_count):
             relation = random.choice(relations)
-            random_time = datetime.today() + random.random() * timedelta(days=1)
+            # Need to login as relation mentee
+            sql = 'SELECT email, password FROM "user" NATURAL JOIN account WHERE "user".userID = %s'
+            data = (relation['menteeid'],)
+            login_details = get_data(sql, data)
+            if not login_user(login_details[0]['email'], login_details[0]['password'], 'mentee'):
+                continue
+                        
+            random_time = fake.date_time_this_century(before_now = False, after_now = True)
             random_end_time = random_time + timedelta(minutes=random.randrange(120))
             if add_meeting(relation, random_time.strftime('%d/%m/%y %H:%M'), random_end_time.strftime('%d/%m/%y %H:%M'), fake.sentences(nb=1), fake.paragraph(nb_sentences=5)):
                 created_meetings += 1
