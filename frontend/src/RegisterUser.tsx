5960e8f2722fc8287acef85ac2983553a0d1237f
Jenny <jenny.hutchins@warwick.ac.uk>
2022-03-04 13:35:25 +0000

    started register user

--- frontend/src/RegisterUser.tsx
+++ frontend/src/RegisterUser.tsx
@@ -1,91 +1,89 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BiUser } from "react-icons/bi";
 import LeftPanel from "./components/LeftPanel";
 import TextInput from "./components/TextInput";
 import Dropdown from "./components/Dropdown";
+import LoginButton from "./components/LoginButton";
+import axios from "axios";
 
 function RegisterUser() {
   const [role, setRole] = React.useState<string>("mentor");
 
-  var x = (
-    <div className="flex flex-col space-y-8 pt-[8%]">
-      <Dropdown
-        values={["mentor", "mentee", "admin"]}
-        labels={["Mentor", "Mentee", "Admin"]}
-        onChange={(e: any) => {
-          setRole(e.target.value);
-        }}
-        icon={<BiUser className="text-4xl m-4 mr-0" />}
-      ></Dropdown>
-    </div>
-  );
-  if (role === "mentor") {
-    x = (
-      <div className="flex flex-col space-y-8 pt-[8%]">
-        <Dropdown
-          values={["mentor", "mentee", "admin"]}
-          labels={["Mentor", "Mentee", "Admin"]}
-          onChange={(e: any) => {
-            setRole(e.target.value);
-          }}
-          icon={<BiUser className="text-4xl m-4 mr-0" />}
-        ></Dropdown>
-        <TextInput
-          type="text"
-          value=""
-          onChange={(e: any) => {}}
-          placeholder="Mentor"
-          icon={<BiUser className="text-4xl m-4 mr-0" />}
-        />
-      </div>
-    );
-  } else if (role === "mentee") {
-    x = (
-      <div className="flex flex-col space-y-8 pt-[8%]">
-        <Dropdown
-          values={["mentor", "mentee", "admin"]}
-          labels={["Mentor", "Mentee", "Admin"]}
-          onChange={(e: any) => {
-            setRole(e.target.value);
-          }}
-          icon={<BiUser className="text-4xl m-4 mr-0" />}
-        ></Dropdown>
-        <TextInput
-          type="text"
-          value=""
-          onChange={(e: any) => {}}
-          placeholder="Mentee"
-          icon={<BiUser className="text-4xl m-4 mr-0" />}
-        />
-        <TextInput
-          type="text"
-          value=""
-          onChange={(e: any) => {}}
-          placeholder="Mentee"
-          icon={<BiUser className="text-4xl m-4 mr-0" />}
-        />
-      </div>
-    );
-  } else if (role === "admin") {
-    x = (<div className="flex flex-col space-y-8 pt-[8%]">
-    <Dropdown
-      values={["mentor", "mentee", "admin"]}
-      labels={["Mentor", "Mentee", "Admin"]}
-      onChange={(e: any) => {
-        setRole(e.target.value);
-      }}
-      icon={<BiUser className="text-4xl m-4 mr-0" />}
-    ></Dropdown>
+  const [bizArea, setBizArea] = React.useState<string>("mentor");
+  const [topics, setTopics] = React.useState<string[]>();
+  const [ratings, setRatings] = React.useState<number[]>();
+
+  useEffect(() => {
+    axios.get("/api/admin/get-skills").then((res: any) => {
+      const skills = res.data;
+      console.log(skills);
+    });
+  }, []);
+
+  const [psword, setPsword] = React.useState<string>("");
+
+  const register = () => {};
+
+  const RegisterAdmin = (
+    <div>
       <TextInput
-        type="text"
-        value=""
-        onChange={(e: any) => {}}
-        placeholder="Admin"
+        type="password"
+        value={psword}
+        onChange={(e: any) => {
+          setPsword(e.target.value);
+        }}
+        placeholder="Admin Password"
         icon={<BiUser className="text-4xl m-4 mr-0" />}
       />
-      </div>
-    );
+    </div>
+  );
+
+  const RegisterMentor = (
+    <div>
+      <TextInput
+        type="password"
+        value={psword}
+        onChange={(e: any) => {
+          setPsword(e.target.value);
+        }}
+        placeholder="Mentor Password"
+        icon={<BiUser className="text-4xl m-4 mr-0" />}
+      />
+    </div>
+  );
+
+  const RegisterMentee = (
+    <div>
+      <TextInput
+        type="password"
+        value={psword}
+        onChange={(e: any) => {
+          setPsword(e.target.value);
+        }}
+        placeholder="Mentee Password"
+        icon={<BiUser className="text-4xl m-4 mr-0" />}
+      />
+    </div>
+  );
+
+  var userFields = <div></div>;
+
+  switch (role) {
+    case "mentor": {
+      userFields = RegisterMentor;
+      break;
+    }
+
+    case "mentee": {
+      userFields = RegisterMentee;
+      break;
+    }
+
+    case "admin": {
+      userFields = RegisterAdmin;
+      break;
+    }
   }
 
   return (
@@ -97,8 +95,23 @@ function RegisterUser() {
         <div className="bg-cultured h-full w-3/5 m-auto pt-[5%] pb-[5%] flex text-prussianBlue overflow-scroll overflow-x-auto">
           <div className="w-3/5 m-auto flex flex-col text-prussianBlue justify-center space-y-10">
             {/* Inputs */}
-            {x}
-            <div className="pt-[32px]"></div>
+            <div className="flex flex-col space-y-8 pt-[8%]">
+              <Dropdown
+                values={["mentor", "mentee", "admin"]}
+                labels={["Mentor", "Mentee", "Admin"]}
+                onChange={(e: any) => {
+                  setRole(e.target.value);
+                }}
+                icon={<BiUser className="text-4xl m-4 mr-0" />}
+              ></Dropdown>
+              {userFields}
+            </div>
+
+            <div className="pt-[32px]">
+              <div className="pt-[32px]">
+                <LoginButton value="Register" onClick={register} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
