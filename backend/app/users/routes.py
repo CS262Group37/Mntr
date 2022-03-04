509ebe22d0d103a2b5482ed9079f3d3c00790243
Mary Kassayova <mkassayova39@gmail.com>
2022-03-04 16:11:02 +0000

    get user data method

--- backend/app/users/routes.py
+++ backend/app/users/routes.py
@@ -1,12 +1,21 @@
 from app.auth.routes import AuthResource
 from . import users_api
+from . import relations_api
 from . import parsers
 from . import users
 
-class GetData(AuthResource):
+class GetOwnData(AuthResource):
     @users_api.doc(security='apiKey')
     def get(self):
         return users.get_data(self.payload['userID'])
+
+class GetUserData(AuthResource):
+    @users_api.doc(security='apiKey')
+    @relations_api.expect(parsers.get_users_parser)
+    def get(self):
+        data = parsers.send_email_parser.parse_args()
+
+        return users.get_data(data['userID'])
 
 class GetTopics(AuthResource):
     @users_api.doc(security='apiKey')
@@ -18,6 +27,7 @@ class GetRatings(AuthResource):
     def get(self):
         return users.get_ratings(self.payload['userID'])
     
-users_api.add_resource(GetData, '/get-data')
+users_api.add_resource(GetOwnData, '/get-own-data')
+users_api.add_resource(GetUserData, '/get-user-data')
 users_api.add_resource(GetTopics, '/get-topics')
 users_api.add_resource(GetRatings, '/get-ratings')
