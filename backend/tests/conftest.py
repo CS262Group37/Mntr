e23cae280540685480ddeda608c4956ab1d4693a
Joshua Manchester <joshuamanchester4@gmail.com>
2022-03-08 18:52:21 +0000

    Added pytest file

---
+++ backend/tests/conftest.py
@@ -0,0 +1,20 @@
+import sys
+
+import pytest
+
+sys.path.append('/.../app/')
+from app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    print("created app")
+
+    # other setup can go here
+
+    yield app
+    
+    # clean up / reset resources here
