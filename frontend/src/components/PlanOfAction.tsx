ca7d50fbc9ade436b1b0d45301c0525d6c64a348
Mary Kassayova <mkassayova39@gmail.com>
2022-03-09 13:19:07 +0000

    Plan of action can be marked as complete

--- frontend/src/components/PlanOfAction.tsx
+++ frontend/src/components/PlanOfAction.tsx
@@ -33,10 +33,8 @@ const ListElem: React.FC<ListElemProps> = (props) => {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
-    // if (goal.status === "complete")
-    //   goal.status = "incomplete";
-    // else
-    //   goal.status = "complete";
+    axios.put("/api/plan/mark-plan-complete", { planID: goal.goalID });
+    console.log("plan complete");
   };
 
 
@@ -67,38 +65,6 @@ const ListElem: React.FC<ListElemProps> = (props) => {
 };
 
 const PlanOfAction: React.FC<PlanProps> = (props) => {
-  // const [goals, setGoals] = React.useState<Goal[]>([]);
-
-  // useEffect(() => {
-  //   let newGoals: Goal[];
-
-  //   axios
-  //     .get("/api/plan/get-plan", { params: { relationID: props.relationID } })
-  //     .then((res: any) => {
-  //       newGoals = res.data.map((g:any) => {
-  //         return {
-  //           goalID: g.goalid,
-  //           title: g.title,
-  //           description: g.description,
-  //           // creationDate?: Date;
-  //           status: g.status,
-  //         }
-  //       });
-  //       console.log(newGoals);  
-
-  //       // dummy data
-  //       setGoals([
-  //         {title: "goal 1", description: "just another goal", status: "complete"},
-  //         {title: "goal 2", description: "just another goal", status: "complete"},
-  //         {title: "goal 3", description: "just another goal", status: "incomplete"},
-  //         {title: "goal 4", description: "just another goal", status: "incomplete"},
-  //         {title: "goal 5", description: "just another goal", status: "incomplete"},
-  //       ]);
-
-  //       setGoals(newGoals);
-  //     });
-  // }, []);
-
   return (
     <div className="flex h-full bg-blueBg bg-cover w-1/3 flex-col text-left fixed right-0 text-cultured">
       <h1 className="text-3xl font-semibold m-10 ml-12 mr-12 mb-8">
