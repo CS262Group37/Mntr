fef2491227fb5c95f5a6fec7d32f2591cd42049c
Joshua Manchester <joshuamanchester4@gmail.com>
2022-03-09 13:32:25 +0000

    Cleaned-up matching code

--- backend/app/matching/routes.py
+++ backend/app/matching/routes.py
@@ -1,15 +1,22 @@
-from app.auth.routes import AuthResource
+from app.auth.auth import AuthResource
 from . import matching_api
 from . import matching
 
+
 class RelationRecommendations(AuthResource):
-    roles = ['mentee']
+    """Get a list of recommended mentors for the logged in mentee.
+
+    Must be logged in as a mentee.
+    """
+
+    roles = ["mentee"]
 
     def get(self):
-        result = matching.get_recommended_mentors(self.payload['userID'])
+        result = matching.get_recommended_mentors(self.payload["userID"])
         if result[0]:
             return result[1], 200
         else:
             return result[1], 500
 
-matching_api.add_resource(RelationRecommendations, '/relation-recommendations')
+
+matching_api.add_resource(RelationRecommendations, "/relation-recommendations")
