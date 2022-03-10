3d3354e67b2eb3ff24e4af726f27d3a2b0853425
Joshua Manchester <joshuamanchester4@gmail.com>
2022-03-10 17:18:13 +0000

    Added GetEmails route

--- backend/app/messages/routes.py
+++ backend/app/messages/routes.py
@@ -3,6 +3,19 @@ from . import messages_api
 from . import messages
 
 
+class GetEmails(AuthResource):
+    """Get all emails for the logged in user.
+
+    Must be logged in as a mentor or mentee.
+    """
+
+    roles = ["mentee", "mentor"]
+
+    @messages_api.doc(security="apiKey")
+    def get(self):
+        return messages.get_emails(self.payload["userID"])
+
+
 class GetMessages(AuthResource):
     """Get all types of messages for the logged in user.
 
