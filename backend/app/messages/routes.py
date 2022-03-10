e77fe3a274b6a21f3ac065e06529b7b6c3c4be82
Joshua Manchester <joshuamanchester4@gmail.com>
2022-03-10 11:59:49 +0000

    Applied black formatting to messages

--- backend/app/messages/routes.py
+++ backend/app/messages/routes.py
@@ -2,15 +2,18 @@ from app.auth.routes import AuthResource
 from . import messages_api
 from . import messages
 
+
 class GetMessages(AuthResource):
     """Get all types of messages for the logged in user.
-    
+
     Must be logged in as a mentor or mentee.
     """
-    roles = ['mentee', 'mentor']
 
-    @messages_api.doc(security='apiKey')
+    roles = ["mentee", "mentor"]
+
+    @messages_api.doc(security="apiKey")
     def get(self):
-        return messages.get_messages(self.payload['userID'])
+        return messages.get_messages(self.payload["userID"])
 
-messages_api.add_resource(GetMessages, '/get_messages')
+
+messages_api.add_resource(GetMessages, "/get_messages")
